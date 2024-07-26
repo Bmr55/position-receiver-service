b52fb23a8ba7360c6a3055a02a920ef7a392b280
Benjamin Runco <Benrunco@gmail.com>
2024-07-26 00:53:49 -0400

    Initialize database and force close open sessions

--- positionrecieverservice.go
+++ positionrecieverservice.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
-
+	"positionrecieverservice/internal/db"
 	"positionrecieverservice/internal/websocket"
 )
 
 func main() {
 	slog.SetLogLoggerLevel(slog.LevelInfo)
+
+	db.InitDB("electrack", "localhost:3306", "user", "pass")
+
+	_, forceCloseErr := db.ForceCloseAllSessions()
+
+	if forceCloseErr != nil {
+		slog.Error(fmt.Sprintf("Failed to Force Open Sessions Closed: %s", forceCloseErr.Error()))
+	}
 
 	websocket.StartServer()
 }
